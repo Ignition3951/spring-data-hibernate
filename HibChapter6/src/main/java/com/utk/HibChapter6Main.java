ade34f0f7cc775de30021b11ad51b3a7fdbf4d34
Utkarsh <ignition3951@gmail.com>
2025-01-04 12:54:34 +0530

    Adding chapter5 module dependencies to chapter 6

---
+++ HibChapter6/src/main/java/com/utk/HibChapter6Main.java
@@ -0,0 +1,13 @@
+package com.utk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibChapter6Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJPATest.class, args);
+	}
+
+}
